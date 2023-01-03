d7c5374bc2960ea83becf226f3af36aa5c50ec1e
HamzaHersi01 <105432295+HamzaHersi01@users.noreply.github.com>
2023-01-03 13:20:39 +0000

    added a few more test cases

--- FlightBookingSystem_Dist/src/bcu/cmp5332/bookingsystem/model/FlightTest.java
+++ FlightBookingSystem_Dist/src/bcu/cmp5332/bookingsystem/model/FlightTest.java
@@ -18,6 +18,16 @@ public class FlightTest {
 		assertEquals(5, stats.getId());
 	}
 
-
-
+	private final Flight stats2 = new Flight(5, "DSE1234", "Dublin", "Moroco",date,200, 40);
+	@Test
+	public void SetIDTest() {
+		stats2.setId(3);
+		assertEquals(3, stats2.getId());
+	}
+	
+	private final Flight stats3 = new Flight(5, "DSE1234", "Dublin", "Moroco",date,200, 40);
+	@Test
+	public void getFlightNumberTest() {
+		assertEquals("DSE1234", stats3.getFlightNumber());
+	}
 }
