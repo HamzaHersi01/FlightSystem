4939933a83617f94cc1b451a11e1ee506a320fbb
HamzaHersi01 <105432295+HamzaHersi01@users.noreply.github.com>
2023-01-03 14:14:14 +0000

    added a few more test cases

--- FlightBookingSystem_Dist/src/bcu/cmp5332/bookingsystem/model/FlightTest.java
+++ FlightBookingSystem_Dist/src/bcu/cmp5332/bookingsystem/model/FlightTest.java
@@ -30,4 +30,50 @@ public class FlightTest {
 	public void getFlightNumberTest() {
 		assertEquals("DSE1234", stats3.getFlightNumber());
 	}
+	
+	private final Flight stats4 = new Flight(5, "DSE1234", "Dublin", "Moroco",date,200, 40);
+	@Test
+	public void setFlightNumberTest() {
+		stats4.setFlightNumber("LLKSH4");
+		assertEquals("LLKSH4", stats4.getFlightNumber());
+	}
+	
+	private final Flight stats5 = new Flight(5, "DSE1234", "Dublin", "Moroco",date,200, 40);
+	@Test
+	public void getOrginTest() {
+		assertEquals("Dublin", stats5.getOrigin());
+	}
+	
+	private final Flight stats6 = new Flight(5, "DSE1234", "Dublin", "Moroco",date,200, 40);
+	@Test
+	public void setOrginTest() {
+		stats6.setOrigin("New York");
+		assertEquals("New York", stats6.getOrigin());
+	}
+	
+	private final Flight stats7 = new Flight(5, "DSE1234", "Dublin", "Moroco",date,200, 40);
+	@Test
+	public void getDestinationTest() {
+		assertEquals("Moroco", stats7.getDestination());
+	}
+	
+	private final Flight stats8 = new Flight(5, "DSE1234", "Dublin", "Moroco",date,200, 40);
+	@Test
+	public void setDestinationTest() {
+		stats8.setDestination("New York");
+		assertEquals("New York", stats8.getDestination());
+	}
+	
+	private final Flight stats9 = new Flight(5, "DSE1234", "Dublin", "Moroco",date,200, 40);
+	@Test
+	public void getDepartureDate() {
+		assertEquals("2022-12-24", stats9.getDepartureDate());
+	}
+	
+	
+	
+	
+	
+	
+	
 }
